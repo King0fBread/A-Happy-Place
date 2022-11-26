041462b99cfb789424cddd745162310690eb538f
Nikita Glubokov <ITVenomancer@gmail.com>
2022-11-26 20:03:23 +0300

    building storyline

---
+++ New to town/Assets/FifthConversationEventsController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FifthConversationEventsController : MonoBehaviour
+{
+    [SerializeField] private RoomDoorBlocker _neighborRoomBlocker;
+
+    [SerializeField] private RoomDoorBlocker _playerRoomBlocker;
+}
---
+++ New to town/Assets/Scripts/NeighborDoorLogic.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NeighborDoorLogic : MonoBehaviour
+{
+    [SerializeField] private RoomDoorBlocker _neighborDoorBlocker;
+    //audio
+    private bool _playerCameToTheDoorFirstTime = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            _playerCameToTheDoorFirstTime = !_playerCameToTheDoorFirstTime;
+
+            if (_playerCameToTheDoorFirstTime)
+            {
+            //first opening sound
+            _neighborDoorBlocker.CanPlayerLeaveRoom(true);
+            }
+            else
+            {
+
+            }
+            gameObject.SetActive(false);
+        }
+    }
+}
