9bf3e680eebc8168514463f8b3c84da31dd3cba8
Nikita Glubokov <ITVenomancer@gmail.com>
2022-12-24 11:20:51 +0300

    Finished final death cutscene; started outro

--- New to town/Assets/Scripts/TimelineActivator.cs
+++ New to town/Assets/Scripts/TimelineActivator.cs
@@ -4,10 +4,14 @@ using UnityEngine.Playables;
 
 public class TimelineActivator : MonoBehaviour
 {
+    [SerializeField] private Animator _playerAnimator;
     [SerializeField] private GameObject _playerUI;
+    [SerializeField] private FlashlightLogic _playerFlashlight;
     private PlayableDirector _director;
+    private BoxCollider _thisCoxCollider;
     private void Awake()
     {
+        _thisCoxCollider = GetComponent<BoxCollider>();
         _director = GetComponent<PlayableDirector>();
         _director.stopped += Director_Stopped;
     }
@@ -15,7 +19,11 @@ public class TimelineActivator : MonoBehaviour
     {
         if (other.gameObject.CompareTag("Player"))
         {
+            _thisCoxCollider.enabled = false;
+            _playerAnimator.enabled = true;
             _director.Play();
+
+            _playerFlashlight.ToggleFlashlightPermisson(false);
             _playerUI.SetActive(false);
         }
     }
