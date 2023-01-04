6f53c67d70278938112ea23178545712c6081b86
Nikita Glubokov <ITVenomancer@gmail.com>
2023-01-04 14:36:51 +0300

    Basic outro finished; working on Menu scene

---
+++ New to town/Assets/Scripts/GameToMenu.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameToMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject _playerUI;
+    [SerializeField] private GameObject _blackScreen;
+    private void OnEnable()
+    {
+        StartCoroutine(MenuTransitionCoroutine());
+    }
+    private IEnumerator MenuTransitionCoroutine()
+    {
+        _playerUI.SetActive(true);
+        _blackScreen.SetActive(true);
+        SoundsManager.instance.PlaySound(SoundsManager.Sounds.EnvironmentMetalDoorOpens);
+        yield return new WaitForSeconds(1);
+        SceneManager.LoadScene(0);
+    }
+}
