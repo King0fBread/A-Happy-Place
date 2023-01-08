ead0d14372b32bf53a95522f1bc00a8e9d5620b1
Nikita Glubokov <ITVenomancer@gmail.com>
2023-01-08 19:35:45 +0300

    Fixed camera jittering

--- New to town/Assets/Scripts/ControllCinemachineShake.cs
+++ New to town/Assets/Scripts/ControllCinemachineShake.cs
@@ -3,19 +3,32 @@ using Cinemachine;
 
 public class ControllCinemachineShake : MonoBehaviour
 {
+    [SerializeField] private float _shakeInterpolationIntensity;
     public float _runningShakeIntensity;
     public float _walkingShakeIntensity;
     public float _idleShakeIntensity;
     private CinemachineVirtualCamera _cinemachineVirtualCamera;
+    private CinemachineBasicMultiChannelPerlin _cinemachineBasicMultiChannelPerlin;
+    private float _currentIntensity;
+    private float _requiredIntensity;
     private void Awake()
     {
         _cinemachineVirtualCamera = GetComponent<CinemachineVirtualCamera>();
     }
     public void StartShake(float intensity)
     {
-        CinemachineBasicMultiChannelPerlin _cinemachineBasicMultiChannelPerlin =
-            _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
+        _cinemachineBasicMultiChannelPerlin = _cinemachineVirtualCamera.GetCinemachineComponent<CinemachineBasicMultiChannelPerlin>();
 
-        _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = intensity;
+        _currentIntensity = _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain;
+        _requiredIntensity = intensity;
+
+        if (_currentIntensity != _requiredIntensity)
+        {
+            _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = Mathf.Lerp(_currentIntensity, _requiredIntensity, _shakeInterpolationIntensity);
+        }
+        else
+        {
+            _cinemachineBasicMultiChannelPerlin.m_AmplitudeGain = _requiredIntensity;
+        }
     }
 }
